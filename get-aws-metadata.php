2bbf33e2b23cc72999849a38f6804537fdce20c4
Natallia Kazarynava <nkazarynava@estee.com>
2025-05-04 21:57:21 +0200

    Working version of php code for getting instance metadata

--- get-aws-metadata.php
+++ get-aws-metadata.php
@@ -1,20 +1,2 @@
 <?php
 require 'vendor/autoload.php';
-
-use Aws\Ec2Metadata\Ec2MetadataClient;
-
-function getAwsMetadataFromSdk() {
-    try {
-        $client = new Ec2MetadataClient();
-        $az = $client->getAvailabilityZone();
-        $region = substr($az, 0, -1);
-        return [
-            'region' => $region,
-            'availability_zone' => $az
-        ];
-    } catch (Exception $e) {
-        return [
-            'error' => 'Could not fetch metadata: ' . $e->getMessage()
-        ];
-    }
-}
